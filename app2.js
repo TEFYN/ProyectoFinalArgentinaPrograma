14de26df7623aa42497b46c1091c82d82f3b8dbc
StefaniaAlaniz <tefy.alaniz@gmail.com>
2023-03-05 23:24:19 -0300

    continuando CV

---
+++ app2.js
@@ -0,0 +1,16 @@
+
+
+// info = document.getElementById("info");
+
+
+
+// griditem = document.getElementById('grid-item');
+
+// griditem.addEventListener('click', ocultar)
+
+// function ocultar(){
+//     // info.className += ' oculto';
+//     info.innerHTML = "Hello ";
+// }
+
+ 
