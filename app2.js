a1067ba9edfc4c2815a5672af59142bbe1fa8e81
StefaniaAlaniz <tefy.alaniz@gmail.com>
2023-03-10 22:01:37 -0300

    continuando CV

--- app2.js
+++ app2.js
@@ -1,16 +1,29 @@
 
 
-// info = document.getElementById("info");
+document.getElementById("grid-item0").addEventListener('mouseover', function() {
+    document.getElementById("nuevoh5").innerHTML = "My name is,"
+    document.getElementById("nuevoh3").innerHTML = "Stefania Alaniz"
+})
+
+document.getElementById("grid-item1").addEventListener('mouseover', function() {
+    document.getElementById("nuevoh5").innerHTML = "My email is,"
+    document.getElementById("nuevoh3").innerHTML = "tefy.alaniz@gmail.com"
+})
 
 
+document.getElementById("grid-item2").addEventListener('mouseover', function() {
+    document.getElementById("nuevoh5").innerHTML = "My birthday is,"
+    document.getElementById("nuevoh3").innerHTML = "15/09/1989"
+})
 
-// griditem = document.getElementById('grid-item');
 
-// griditem.addEventListener('click', ocultar)
+document.getElementById("grid-item3").addEventListener('mouseover', function() {
+    document.getElementById("nuevoh5").innerHTML = "My location is,"
+    document.getElementById("nuevoh3").innerHTML = "Córdoba, Argentina"
+})
 
-// function ocultar(){
-//     // info.className += ' oculto';
-//     info.innerHTML = "Hello ";
-// }
 
- 
+document.getElementById("grid-item4").addEventListener('mouseover', function() {
+    document.getElementById("nuevoh5").innerHTML = "My phone is,"
+    document.getElementById("nuevoh3").innerHTML = "351 7074353"
+})
